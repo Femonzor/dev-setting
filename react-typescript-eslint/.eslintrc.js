d197db1c6fb667f120d07d9389b87ac1b9c982a0
Femonzor <wayneyang3@gmail.com>
2020-03-22 19:42:27 +0800

    react-typescript-eslint

---
+++ react-typescript-eslint/.eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'react-app',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint', 'react'],
+  rules: {
+    'prettier/prettier': [
+      'error', {
+        'singleQuote': true,
+      },
+    ],
+  },
+};
